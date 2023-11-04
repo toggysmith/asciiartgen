0003b906fbc270668d74a3693ef352a9c0c0a256
Toggy Smith <toggysmith@gmail.com>
2023-11-04 22:18:01 +0000

    Load image based on argument values

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 #include <SFML/Graphics.hpp>
 
-int main() {
+/**
+ * @brief Validate whether the arguments meet the expected format.
+ * 
+ * The arguments are expected to be in the format `-f X` where `-f`
+ * indicates the second argument is a filename and `X` is the
+ * filename.
+ * 
+ * @param arguments The arguments.
+ * @return Whether or not the arguments are valid.
+*/
+const bool validate_arguments(const std::vector<std::string> arguments) {
+    const bool are_there_two_arguments{ arguments.size() == 2 };
+    
+    if (!are_there_two_arguments) {
+        std::cerr << "There should be two arguments.\n";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // WHY: It's safer + more convenient to deal with a C++ container.
+    std::vector<std::string> arguments(argv, (argv) + argc);
+
+    // WHY: We don't want the first 'argument' which is always the executable name.
+    arguments.erase(arguments.begin());
+
+    if (!validate_arguments(arguments)) {
+        std::exit(EXIT_FAILURE);
+    }
+
+    for (const auto argument : arguments) {
+        std::cout << argument << "\n";
+    }
+
     sf::Image image;
 
-    image.loadFromFile("./data/test_image.png");
+    image.loadFromFile(arguments[1]);
 
     std::cout << image.getSize().x << " x " << image.getSize().y << std::endl;
 
@@ -12,5 +50,5 @@ int main() {
     std::cout << "G: " << static_cast<int>(image.getPixel(0, 0).g) << std::endl;
     std::cout << "B: " << static_cast<int>(image.getPixel(0, 0).b) << std::endl;
 
-    return 0;
+    std::exit(EXIT_SUCCESS);
 }
