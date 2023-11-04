8fcde5e7886793af1206eff96510322191fc3803
Toggy Smith <toggysmith@gmail.com>
2023-11-04 21:17:57 +0000

    Load a test image to demonstrate image loading capability
    This adds SFML 2.6.1 as a dependency and uses it to load a test
    image file. The only SFML modules that are built are the Graphics
    module (for image loading capability) and the Windows module
    (must be included for the Graphics module). This also changes the
    CMake to copy across a `data` directory from the source directory
    to the binary directory where the executable is located, so that
    the executable can have easy access to the normal location of the
    test image file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, world!\n";
+    sf::Image image;
+
+    image.loadFromFile("./data/test_image.png");
+
+    std::cout << image.getSize().x << " x " << image.getSize().y << std::endl;
+
+    std::cout << "R: " << static_cast<int>(image.getPixel(0, 0).r) << std::endl;
+    std::cout << "G: " << static_cast<int>(image.getPixel(0, 0).g) << std::endl;
+    std::cout << "B: " << static_cast<int>(image.getPixel(0, 0).b) << std::endl;
 
     return 0;
 }
